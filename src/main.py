6ddc5fcda3047b44b34c34a185ba319cdf699904
Pedro Martinez <pedro.enrique.83@gmail.com>
2025-11-29 23:41:40 -0700

    add cmd loop

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import cmd
 from typing import TextIO
@@ -7,16 +8,41 @@ class main_cli(cmd.Cmd):
     def __init__(self, completekey: str = "tab", stdin: TextIO | None = None, stdout: TextIO | None = None) -> None:
         super().__init__(completekey, stdin, stdout)
 
-    def f2wl(self, arg: str) -> None:
+    def do_f2wl(self, arg: str) -> None:
+        """Frequency to wavelength"""
         parser = argparse.ArgumentParser(description="Convert frequency to wavelength")
+        parser.add_argument('frequency', help="Frequency")
+        parser.add_argument('unit')
+        args = parser.parse_args()
     
     
-    def wl2f(self, arg: str) -> None:
+    def do_wl2f(self, arg: str) -> None:
+        """Wavelength to frequency"""
         parser = argparse.ArgumentParser(description="Convert wavelength to frequency")
+        parser.add_argument('wavelength', help="Wavelength")
+        parser.add_argument('unit')
+        args = parser.parse_args()
 
 
-    def db2pl(self, arg: str) -> None:
+    def do_db2pl(self, arg: str) -> None:
+        """dB to Percentage loss"""
         parser = argparse.ArgumentParser(description="Convert dB to power loss in percentage")
+        parser.add_argument('decibel', help="Decibel")
+        parser.add_argument('unit')
+        args = parser.parse_args()
 
-    def pl2db(self, arg: str) -> None:
+
+    def do_pl2db(self, arg: str) -> None:
+        """Percentage loss to dB"""
         parser = argparse.ArgumentParser(description="Convert power loss in percentage to dB")
+        parser.add_argument('percentage',  help="Percentage loss")
+        parser.add_argument('unit')
+        args = parser.parse_args()
+
+    def do_exit(self, arg: str) -> None:
+        """Exit the application"""
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main_cli().cmdloop()
