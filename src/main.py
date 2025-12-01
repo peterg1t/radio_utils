552e7e1b7308e0d5742dfb7123cee5a9c42074a8
Pedro Martinez <pedro.enrique.83@gmail.com>
2025-11-30 21:33:32 -0700

    finish first version

--- src/main.py
+++ src/main.py
@@ -2,6 +2,34 @@ import sys
 import argparse
 import cmd
 from typing import TextIO
+import math
+
+
+
+def rescale(freq, unit):
+    if unit == 'k':
+        return freq*1000
+    elif unit == 'M':
+        return freq*1000000
+    elif unit == 'G':
+        return freq*1000000000
+    return freq
+
+# Function to convert frequency to wavelength
+def frequency_to_wavelength(frequency: float) -> float:
+    # Speed of light in m/s
+    c = 299792458
+    # Calculate the wavelength
+    wavelength = c / frequency
+    return wavelength
+
+# Function to convert wavelength to frequency
+def wavelength_to_frequency(wavelength: float) -> float:
+    # Speed of light in m/s
+    c = 299792458
+    # Calculate the wavelength
+    freq_hz = c / wavelength
+    return freq_hz
 
 
 class main_cli(cmd.Cmd):
@@ -12,9 +40,12 @@ class main_cli(cmd.Cmd):
         """Frequency to wavelength"""
         parser = argparse.ArgumentParser(description="Convert frequency to wavelength")
         parser.add_argument('frequency', help="Frequency")
-        parser.add_argument('unit')
+        parser.add_argument('unit', choices=['Hz', 'k', 'M', 'G'])
         try:
-            args = parser.parse_args()
+            args = parser.parse_args(arg.split())
+            frequency = rescale(float(args.frequency), args.unit)
+            wavelength = frequency_to_wavelength(frequency)
+            print(f"{wavelength:.4f} m")
         except SystemExit:
             pass
     
@@ -22,10 +53,22 @@ class main_cli(cmd.Cmd):
     def do_wl2f(self, arg: str) -> None:
         """Wavelength to frequency"""
         parser = argparse.ArgumentParser(description="Convert wavelength to frequency")
-        parser.add_argument('wavelength', help="Wavelength")
-        parser.add_argument('unit')
+        parser.add_argument('wavelength', help="Wavelength in meters")
         try:
-            args = parser.parse_args()
+            args = parser.parse_args(arg.split())
+            wavelength = float(args.wavelength)
+            frequency = wavelength_to_frequency(wavelength)
+            if frequency >= 1e9:
+                print(f"{frequency / 1e9:.6g} GHz")
+
+            elif frequency >= 1e6:
+                print(f"{frequency / 1e6:.6g} MHz")
+
+            elif frequency >= 1e3:
+                print(f"{frequency / 1e3:.6g} kHz")
+            
+            else: 
+                print(f"{frequency:.6g} Hz")
         except SystemExit:
             pass
 
@@ -34,9 +77,11 @@ class main_cli(cmd.Cmd):
         """dB to Percentage loss"""
         parser = argparse.ArgumentParser(description="Convert dB to power loss in percentage")
         parser.add_argument('decibel', help="Decibel")
-        parser.add_argument('unit')
         try:
-            args = parser.parse_args()
+            args = parser.parse_args(arg.split())
+            db = float(args.decibel)
+            perloss = 100 * 10 ** (db/20)
+            print(f"{perloss:.4f} %")
         except SystemExit:
             pass
 
@@ -45,9 +90,11 @@ class main_cli(cmd.Cmd):
         """Percentage loss to dB"""
         parser = argparse.ArgumentParser(description="Convert power loss in percentage to dB")
         parser.add_argument('percentage',  help="Percentage loss")
-        parser.add_argument('unit')
         try:
-            args = parser.parse_args()
+            args = parser.parse_args(arg.split())
+            perloss = float(args.percentage)
+            db = 20*math.log10(perloss/100)
+            print(f"{db:.4f} dB")
         except SystemExit:
             pass
 
