d439e40614735e5b7fd0a049725eb0029ac02e29
Pedro Martinez <pedro.enrique.83@gmail.com>
2026-01-18 07:46:01 -0700

    Add requirements with plotext

--- src/loops.py
+++ src/loops.py
@@ -1,20 +1,8 @@
 import math
 
-def radiation_resistance_loop():
-    pass
 
-def radiation_efficiency(r_rad: float, r_loop: float, r_e: float) -> float:
-    """_summary_
-
-    Args:
-        r_rad (float): _description_
-        r_loop (float): _description_
-        r_e (float): in Ohms
-
-    Returns:
-        _type_: _description_
-    """
-    return r_rad/(r_rad + r_loop + r_e)
+def perimeter(radius: float) -> float:
+    return 2*math.pi*radius
 
 def surface_resistance(freq: float) -> float:
     """R_s
@@ -28,8 +16,20 @@ def surface_resistance(freq: float) -> float:
     sigma = 5.8*10**(7) #Siemens/m
     return math.sqrt((2*math.pi*freq*mu_0)/(2*sigma))
 
-def radiation_loss(R_s: float):
-    return = N *(l_perim/r_conductor)*R_s*(R_p/R_0+1)
+def radiation_loss(N: int, R_s: float, r_conductor: float, R_p: float, R_0: float):
+    l_perim = perimeter(r_conductor)
+    return N *(l_perim/r_conductor)*R_s*(R_p/R_0+1)
 
+def radiation_efficiency(r_rad: float, r_loop: float, r_e: float) -> float:
+    """_summary_
 
+    Args:
+        r_rad (float): _description_
+        r_loop (float): _description_
+        r_e (float): in Ohms
+
+    Returns:
+        _type_: _description_
+    """
+    return r_rad/(r_rad + r_loop + r_e)
 
