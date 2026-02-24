abeb0509f14b7d567672d656661cc1ab40455056
Pedro Martinez <pedro.enrique.83@gmail.com>
2026-02-24 12:51:32 -0700

    fix launcher

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,13 @@
-if [ ! -d env ]; then
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+
+if [ ! -d $SCRIPT_DIR/env ]; then
 	echo "Installing dependencies for the first launch..."
-	python3.12 -m venv env
-	source env/bin/activate
-	pip install -r requirements.txt
+	python3.12 -m venv $SCRIPT_DIR/env
+	source $SCRIPT_DIR/env/bin/activate
+	pip install -r $SCRIPT_DIR/requirements.txt
 fi
-source env/bin/activate
-python src/main.py
+source $SCRIPT_DIR/env/bin/activate
+python $SCRIPT_DIR/src/main.py
